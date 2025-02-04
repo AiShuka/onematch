a51a93218d4853f45c34a0910c4e802497cc9e6f
AiShuka <faxasun@gmail.com>
2025-02-04 22:24:09 +0900

    ユーザー管理機能

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem 'devise'
 gem 'bootstrap', '~> 5.2.3'
 
 gem 'jquery-rails'
+
+gem 'active_hash'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefecture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :prefecture_id, presence: true
 end
